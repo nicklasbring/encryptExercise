a10adbb266a85042eeac9966a44b5aa8626208d4
KugelbergDK <kugelberg.dk@gmail.com>
2019-09-13 09:46:32 +0200

    Added Cipher constructor, where it generates the shift length from parameter

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,14 +1,33 @@
 public class Cipher {
 
     private String key;
+    private int shift;
 
 
-    public String encryptText(){
-
+    public Cipher(String key){
+        // Generate shift length from input key
+        if (key.length() > 26) {
+            if (key.length() / 2 > 26) {
+                this.shift = key.length() / 3;
+            }
+            else {
+                this.shift = key.length() / 2;
+            }
+        }
+        else {
+            this.shift = key.length();
+        }
     }
 
-    public String decryptText(){
+    public String encrypt(String msg){
+        // Convert string to charArray
+        char[] chars = msg.toCharArray();
 
+        return null;
+    }
+
+    public String decrypt(){
+        return null;
     }
 
 
